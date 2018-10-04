ab4df720ab582e25609105538a958c2ccd78029f
Rudy Ravelin <rudy@noukod.com>
2018-10-04 15:53:32 +0000

    automatically backed up by learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,15 +1,18 @@
-class Post
-
-  attr_accessor :title, :author
-  def initialize(title)
-    @title = title
-  end
-
-  def author_name
-    if @author == nil
-      return nil
-    else
-      @author.name
-    end
-  end
+class Post	
+  attr_accessor :title, :author	
+   @@all = []	
+   def initialize(title)	
+    @title = title	
+    @@all << self	
+  end	
+   def self.all	
+    @@all	
+  end	
+   def author_name	
+    if self.author	
+      self.author.name	
+    else	
+      nil	
+    end	
+  end	
 end
