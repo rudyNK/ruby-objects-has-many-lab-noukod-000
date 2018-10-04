c4437c3e6f36735d32e7d0d01e45a3998e21c00c
Rudy Ravelin <rudy@noukod.com>
2018-10-04 15:49:25 +0000

    automatically backed up by learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -1,25 +1,31 @@
+require 'pry'
+
 class Artist
-  attr_accessor :name
-   @@song_count = 0
-   def initialize(name)
+
+  attr_accessor :name, :songs
+
+  @@song_count = 0
+
+  def initialize(name)
     @name = name
     @songs = []
   end
-   def add_song(song)
-    self.songs << song
+
+  def add_song(song)
+    @songs << song
     song.artist = self
-    @@song_count +=1
+    @@song_count += 1
   end
-   def add_song_by_name(name)
+
+  def add_song_by_name(name)
     song = Song.new(name)
-    self.songs << song
+    @songs << song
     song.artist = self
-    @@song_count +=1
+    @@song_count += 1
   end
-   def songs
-    @songs
-  end
-   def self.song_count
+
+  def self.song_count
     @@song_count
   end
-end 
+
+end
