1147f5c4ae68745820856bb7954987eb2ff59624
Rudy Ravelin <rudy@noukod.com>
2018-10-04 15:49:19 +0000

    automatically backed up by learn

--- lib/author.rb
+++ lib/author.rb
@@ -1,25 +1,28 @@
 class Author
-  attr_accessor :name
-   @@post_count = 0
-   def initialize(name)
+
+  attr_accessor :name, :posts
+
+  @@post_count = 0
+
+  def initialize(name)
     @name = name
     @posts = []
   end
-   def add_post(post)
-    self.posts << post
+
+  def add_post(post)
+    @posts << post
     post.author = self
-    @@post_count +=1
+    @@post_count += 1
   end
-   def add_post_by_title(title)
+
+  def add_post_by_title(title)
     post = Post.new(title)
-    self.posts << post
+    @posts << post
     post.author = self
-    @@post_count +=1
+    @@post_count += 1
   end
-   def posts
-    @posts
-  end
-   def self.post_count
+
+  def self.post_count
     @@post_count
   end
-end 
+end
